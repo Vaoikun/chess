292237d06d10bc8cd170f5dc05c56231d20d37bb
Vaoikun <furious_sesame_0o@icloud.com>
2024-11-18 19:45:20 -0700

    main added prelogin

--- client/src/main/java/Main.java
+++ client/src/main/java/Main.java
@@ -1,8 +1,11 @@
 import chess.*;
+import ui.PreloginUI;
 
 public class Main {
     public static void main(String[] args) {
         var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
+        PreloginUI prelogin = new PreloginUI("http://localhost:8080");
+        prelogin.run();
         System.out.println("♕ 240 Chess Client: " + piece);
     }
 }
