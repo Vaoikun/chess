b60496feb97473f4d2b16f06f7d405349c276910
Vaoikun <furious_sesame_0o@icloud.com>
2024-12-03 00:03:12 -0700

    michael jackson

--- server/src/main/java/server/websocket/ConnectionManager.java
+++ server/src/main/java/server/websocket/ConnectionManager.java
@@ -4,6 +4,7 @@ import com.mysql.cj.exceptions.CJOperationNotSupportedException;
 import model.GameData;
 import org.eclipse.jetty.websocket.api.Session;
 
+import java.io.IOException;
 import java.util.Objects;
 import java.util.Vector;
 import java.util.concurrent.ConcurrentHashMap;
@@ -31,6 +32,22 @@ public class ConnectionManager {
             for(int i = game.size() - 1; i >= 0; i--){
                 if(Objects.equals(game.get(i).authToken, authToken)){
                     game.remove(i);
+                }
+            }
+        }
+    }
+
+    public void broadcast(int gameID, Session session, String notification) throws IOException {
+        var removeList = new Vector<Connection>();
+        if (CONNECTION.get(gameID) != null) {
+            Vector<Connection> games = CONNECTION.get(gameID);
+            for (Connection connection : games) {
+                if(connection.session.isOpen()){
+                    if(!connection.session.equals(session)){
+                        connection.send(notification);
+                    }
+                }else{
+                    removeList.add(connection);
                 }
             }
         }
