76992a545df8e0241158ea9345a40ba4a72cc65a
Vaoikun <furious_sesame_0o@icloud.com>
2025-07-08 20:48:28 -0600

    null error fixed

--- shared/src/main/java/chess/ChessGame.java
+++ shared/src/main/java/chess/ChessGame.java
@@ -126,12 +126,14 @@ public class ChessGame {
      * @return True if the specified team is in check　
      */
     public boolean isInCheck(TeamColor teamColor) {
-        Collection<ChessMove> legalMoves = new ArrayList<>();
         ChessPosition kingsPosition = kingsPosition(teamColor);
         for (int row = 0; row < 8; row++){
             for (int col = 0; col < 8; col++){
                 ChessPosition currentPosition = new ChessPosition(row + 1, col + 1);
                 ChessPiece currentPiece = this.board.getPiece(currentPosition);
+                if (currentPiece == null) {
+                    continue;
+                }
                 if (currentPiece.getTeamColor() != teamColor) {
                     Collection<ChessMove> opponentMoves = currentPiece.pieceMoves(this.board, currentPosition);
                     return checkMove(opponentMoves, kingsPosition);
@@ -155,6 +157,9 @@ public class ChessGame {
             for (int col = 0; col < 8; col++){
                 ChessPosition currentPosition = new ChessPosition(row + 1, col + 1);
                 ChessPiece currentPiece = this.board.getPiece(currentPosition);
+                if (currentPiece == null) {
+                    continue;
+                }
                 if (currentPiece.getPieceType() == ChessPiece.PieceType.KING && currentPiece.getTeamColor() == teamColor) {
                     return currentPosition;
                 }
