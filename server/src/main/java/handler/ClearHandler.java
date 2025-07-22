55cd023138eca68f26494d4a317cc50009064055
Vaoikun <furious_sesame_0o@icloud.com>
2025-07-21 20:19:19 -0600

    basehandler

--- server/src/main/java/handler/ClearHandler.java
+++ server/src/main/java/handler/ClearHandler.java
@@ -1,4 +1,19 @@
 package handler;
 
-public class ClearHandler {
+import dataaccess.DataAccessException;
+import server.ServerException;
+import spark.Request;
+import spark.Response;
+
+public class ClearHandler extends BaseHandler {
+
+    public ClearHandler (Request request, Response response) {
+        super(request, response);
+    }
+
+    @Override
+    public Object httpHandlerRequest(Request request, Response response) throws ServerException, DataAccessException {
+        return null;
+    }
+
 }
