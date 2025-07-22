017f297f4a5ca1b36151eaf5f235ce89241ccfec
Vaoikun <furious_sesame_0o@icloud.com>
2025-07-22 13:56:19 -0600

    clear test

--- server/src/test/java/service/UnitTest.java
+++ server/src/test/java/service/UnitTest.java
@@ -3,11 +3,22 @@ package service;
 import dataaccess.DataAccessException;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import server.ServerException;
+import static org.junit.jupiter.api.Assertions.*;
+
+import javax.xml.crypto.Data;
 
 public class UnitTest {
     private String authToken;
 
     public UnitTest() throws DataAccessException {
-
     }
+
+    @Test
+    @Order(1)
+    public void clear() throws ServerException, DataAccessException {
+        UserService clearServiceTest = new UserService();
+        assertDoesNotThrow(clearServiceTest::clear);
+    }
+
 }
