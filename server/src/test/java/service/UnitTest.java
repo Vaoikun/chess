078c6f186913d7d25467d0330d22140d9ec5791f
Vaoikun <furious_sesame_0o@icloud.com>
2025-07-21 19:27:30 -0600

    dataaccessobjects

---
+++ server/src/test/java/service/UnitTest.java
@@ -0,0 +1,13 @@
+package service;
+
+import dataaccess.DataAccessException;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+
+public class UnitTest {
+    private String authToken;
+
+    public UnitTest() throws DataAccessException {
+
+    }
+}
