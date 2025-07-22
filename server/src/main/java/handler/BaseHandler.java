cfddfac30a90ae65c6d9fc584ab24206e939b385
Vaoikun <furious_sesame_0o@icloud.com>
2025-07-21 20:12:15 -0600

    basehandler

--- server/src/main/java/handler/BaseHandler.java
+++ server/src/main/java/handler/BaseHandler.java
@@ -1,11 +1,33 @@
 package handler;
 
+import dataaccess.DataAccessException;
+import server.ServerException;
 import spark.Request;
 import spark.Response;
+import com.google.gson.Gson;
 
 
 public abstract class BaseHandler {
     private Request request;
     private Response response;
+
+    public abstract Object httpHandlerRequest(Request request, Response response)
+            throws ServerException, DataAccessException;
+
+    BaseHandler(Request request, Response response) {
+        this.request = request;
+        this.response = response;
+    }
+
+    protected static <T> T getBody (Request request, Class<T> aClass) {
+        Gson gson = new Gson();
+        String json = request.body();
+        T body = gson.fromJson(json, aClass);
+        if (body == null) {
+            throw new RuntimeException("No request body found.");
+        }else{
+            return body;
+        }
+    }
 
 }
