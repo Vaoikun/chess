0e8a59f7e3a7aff207eff6965d6b21c2d7838a5a
Vaoikun <furious_sesame_0o@icloud.com>
2025-07-22 11:50:24 -0600

    clear methods

--- server/src/main/java/handler/ClearHandler.java
+++ server/src/main/java/handler/ClearHandler.java
@@ -2,6 +2,7 @@ package handler;
 
 import com.google.gson.Gson;
 import dataaccess.DataAccessException;
+import httpresponse.MessageResponse;
 import server.ServerException;
 import service.UserService;
 import spark.Request;
@@ -19,7 +20,17 @@ public class ClearHandler extends BaseHandler {
         String body;
         try {
             UserService clearService = new UserService();
+            clearService.clear();
+            body = new Gson().toJson(new MessageResponse(""));
+            response.status(200);
+        } catch (DataAccessException e) {
+            body = gson.toJson(new MessageResponse(e.getMessage()));
+        } catch (ServerException e) {
+            body = new Gson().toJson(new MessageResponse(e.getMessage()));
+            response.status(200);
         }
+        response.type("application/type");
+        return body;
     }
 
 }
