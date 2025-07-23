fd10a8885028c25ec38dca0bddc3e9a65a5fdcd9
Vaoikun <furious_sesame_0o@icloud.com>
2025-07-23 04:33:50 -0600

    resource rooting

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -9,14 +9,13 @@ public class Server {
         Spark.port(desiredPort);
 
         Spark.staticFiles.location("web");
-
+        Spark.delete("/db", (request, response) -> new ClearHandler(request, response).httpHandlerRequest(request, response));
         Spark.post("/user", (request, response) -> new RegisterHandler(request, response).httpHandlerRequest(request, response));
         Spark.post("/session", (request, response) -> new LoginHandler(request, response).httpHandlerRequest(request, response));
         Spark.delete("/session", (request, response) -> new LogoutHandler(request, response).httpHandlerRequest(request, response));
         Spark.get("/game", (request, response) -> new ListGameHandler(request, response).httpHandlerRequest(request, response));
         Spark.post("/game", (request, response) -> new CreateGameHandler(request, response).httpHandlerRequest(request, response));
         Spark.put("/game", (request, response) -> new JoinGameHandler(request, response).httpHandlerRequest(request, response));
-        Spark.delete("/db", (request, response) -> new ClearHandler(request, response).httpHandlerRequest(request, response));
 
         Spark.awaitInitialization();
         return Spark.port();
