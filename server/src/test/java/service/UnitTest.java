4b95181936761ece6dcc6614e3fccfa3aba8bc26
Vaoikun <furious_sesame_0o@icloud.com>
2025-07-22 21:23:09 -0600

    registers

--- server/src/test/java/service/UnitTest.java
+++ server/src/test/java/service/UnitTest.java
@@ -1,6 +1,9 @@
 package service;
 
+import dataaccess.ClientException;
 import dataaccess.DataAccessException;
+import httprequest.RegisterRequest;
+import httpresponse.RegisterResponse;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import server.ServerException;
@@ -10,6 +13,9 @@ import javax.xml.crypto.Data;
 
 public class UnitTest {
     private String authToken;
+    ///test2
+    private final UserService registerService1 = new UserService();
+    private final RegisterRequest registerRequest1 = new RegisterRequest("Mole", "rat", "molerat@email.com");
 
     public UnitTest() throws DataAccessException {
     }
@@ -20,5 +26,24 @@ public class UnitTest {
         UserService clearServiceTest = new UserService();
         assertDoesNotThrow(clearServiceTest::clear);
     }
+
+    @Test
+    @Order(2)
+    public void registerSuccess() throws ServerException, DataAccessException, ClientException {
+        RegisterResponse registerResponse = registerService1.register(registerRequest1);
+        String username = registerResponse.username();
+        assertEquals(registerResponse.username(), username);
+        authToken = registerResponse.authToken();
+    }
+
+    @Test
+    @Order(3)
+    public void registerFailed() throws ServerException, DataAccessException, ClientException {
+        RegisterRequest missingPassword = new RegisterRequest("Mike", null, "mike@email.com");
+        ClientException exception = assertThrows(ClientException.class, () -> registerService1.register(missingPassword));
+        assertEquals(exception.getMessage(), "must set the password.");
+    }
+
+
 
 }
