37bea23bc4b4cfee3d2d178fed4e2ead6a941bd9
Vaoikun <furious_sesame_0o@icloud.com>
2025-07-22 22:33:18 -0600

    loginnout

--- server/src/main/java/handler/LoginHandler.java
+++ server/src/main/java/handler/LoginHandler.java
@@ -1,4 +1,39 @@
 package handler;
 
-public class LoginHandler {
+import com.google.gson.Gson;
+import dataaccess.DataAccessException;
+import httprequest.LoginRequest;
+import httpresponse.LoginResponse;
+import httpresponse.MessageResponse;
+import org.eclipse.jetty.security.LoginService;
+import server.ServerException;
+import service.UserService;
+import spark.Request;
+import spark.Response;
+
+public class LoginHandler extends BaseHandler {
+    public LoginHandler(Request request, Response response) throws DataAccessException, ServerException {
+        super(request, response);
+    }
+    @Override
+    public Object httpHandlerRequest(Request request, Response response) {
+        Gson gson = new Gson();
+        String json;
+        try {
+            final UserService loginService = new UserService();
+            LoginRequest body = getBody(request, LoginRequest.class);
+            LoginResponse loginResponse = loginService.login(body);
+            response.status(200);
+            json = gson.toJson(loginResponse);
+        } catch (ServerException e) {
+            response.status(500);
+            json = gson.toJson(new MessageResponse(e.getMessage()));
+        } catch (DataAccessException e) {
+            response.status(401);
+            json = gson.toJson(new MessageResponse(e.getMessage()));
+        }
+        response.type("application/json");
+        return json;
+    }
+
 }
--- server/src/main/java/handler/LogoutHandler.java
+++ server/src/main/java/handler/LogoutHandler.java
@@ -1,4 +1,37 @@
 package handler;
 
-public class LogoutHandler {
+import com.google.gson.Gson;
+import dataaccess.DataAccessException;
+import httprequest.LoginRequest;
+import httpresponse.LoginResponse;
+import httpresponse.MessageResponse;
+import server.ServerException;
+import service.UserService;
+import spark.Request;
+import spark.Response;
+
+public class LogoutHandler extends BaseHandler{
+    public LogoutHandler(Request request, Response response) throws DataAccessException, ServerException {
+        super(request, response);
+    }
+    @Override
+    public Object httpHandlerRequest(Request request, Response response) {
+        Gson gson = new Gson();
+        String json;
+        String authToken = request.headers("Authorization");
+        try {
+            final UserService logoutService = new UserService();
+            logoutService.logout(authToken);
+            response.status(200);
+            json = new Gson().toJson(new MessageResponse(""));
+        } catch (ServerException e) {
+            response.status(500);
+            json = gson.toJson(new MessageResponse(e.getMessage()));
+        } catch (DataAccessException e) {
+            response.status(401);
+            json = gson.toJson(new MessageResponse(e.getMessage()));
+        }
+        response.type("application/json");
+        return json;
+    }
 }
