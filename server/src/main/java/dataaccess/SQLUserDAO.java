4b59591170635d560f6e9081dedd8798e3ec9153
Vaoikun <furious_sesame_0o@icloud.com>
2025-07-25 01:27:22 -0600

    usertable

--- server/src/main/java/dataaccess/SQLUserDAO.java
+++ server/src/main/java/dataaccess/SQLUserDAO.java
@@ -1,4 +1,33 @@
 package dataaccess;
 
-public class SQLUserDAO {
+import javax.xml.crypto.Data;
+import java.sql.SQLException;
+
+public class SQLUserDAO implements UserDAO{
+    private static String CREATE_STATEMENT =
+            """
+                    CREATE TABLE IF NOT EXISTS Users (
+                    passwordCol varchar(255) NOT NULL,
+                    usernameCol varchar(255) NOT NULL,
+                    emailCol varchar(255) NOT NULL,
+                    PRIMARY KEY (usernameCol))
+                    """;
+
+    public SQLUserDAO() throws DataAccessException {
+        createUserTable();
+    }
+
+    public static void createUserTable() throws DataAccessException {
+        try (var connect =DatabaseManager.getConnection()){
+            try (var createStatement = connect.prepareStatement(CREATE_STATEMENT)) {
+                createStatement.executeUpdate();
+            } catch (SQLException e) {
+                throw new DataAccessException(e.getMessage());
+            }
+        } catch (SQLException | DataAccessException e) {
+            throw new DataAccessException(e.getMessage());
+        }
+    }
+
+
 }
