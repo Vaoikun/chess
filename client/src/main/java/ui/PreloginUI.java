85c90b7a8108455543c7ec0da6cca9d1ff882250
Vaoikun <furious_sesame_0o@icloud.com>
2025-07-28 22:53:13 -0600

    client setup

--- client/src/main/java/ui/PreloginUI.java
+++ client/src/main/java/ui/PreloginUI.java
@@ -1,4 +1,14 @@
 package ui;
 
 public class PreloginUI {
+
+
+    public static String help() {
+        return """
+                   To create an account: Register (USERNAME) (PASSWORD) (EMAIL)
+                   To login into an account: Login (USERNAME) (PASSWORD)
+                   To exit the server: Quit
+                   To show commands: Help
+                   """;
+    }
 }
