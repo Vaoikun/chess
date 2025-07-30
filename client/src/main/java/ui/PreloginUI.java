1781fccb62ac88eba59f19aeb6bf700693973728
Vaoikun <furious_sesame_0o@icloud.com>
2025-07-30 01:32:24 -0600

    prelogin methods

--- client/src/main/java/ui/PreloginUI.java
+++ client/src/main/java/ui/PreloginUI.java
@@ -1,7 +1,13 @@
 package ui;
 
+import httpresponse.LoginResponse;
+import httpresponse.MessageResponse;
+import httpresponse.RegisterResponse;
+
+import java.io.IOException;
 import java.io.PrintStream;
 import java.nio.charset.StandardCharsets;
+import java.util.Objects;
 import java.util.Scanner;
 
 public class PreloginUI {
@@ -12,19 +18,76 @@ public class PreloginUI {
     }
 
     public void run() {
-
+        OUT.println("Welcome to the Chess server!");
+        OUT.println();
+        OUT.println(help());
+        OUT.println();
+        OUT.println("Enter command below.");
+        String input = SCANNER.nextLine();
+        while (!Objects.equals(input, "Quit")) {
+            this.eval(input);
+            OUT.println("Enter command below.");
+            input = SCANNER.nextLine();
+        }
     }
 
     public static void register() {
-
+        OUT.println("Set username below.");
+        String username = SCANNER.nextLine();
+        OUT.println("Set password below.");
+        String password = SCANNER.nextLine();
+        OUT.println("Set email below.");
+        String email = SCANNER.nextLine();
+        try {
+            Object registerResult = ServerFacade.register(username, password, email);
+            if (registerResult instanceof RegisterResponse registerResponse) {
+                String authToken = registerResponse.authToken();
+                PostloginUI postloginUI = new PostloginUI("http://localhost:8080", authToken);
+                OUT.println("Registration successful!");
+                OUT.println();
+                OUT.println("Logging you in...");
+                postloginUI.run();
+            } else {
+                MessageResponse messageResponse = (MessageResponse) registerResult;
+                OUT.println(messageResponse.message());
+                OUT.println();
+                OUT.println(help());
+                OUT.println();
+                OUT.println("Enter command below.");
+            }
+        } catch (IOException e) {
+            OUT.println(e.getMessage());
+        }
     }
 
     public static void login() {
-
+        OUT.println("Enter username below.");
+        String username = SCANNER.nextLine();
+        OUT.println("Enter password below.");
+        String password = SCANNER.nextLine();
+        try {
+            Object loginResult = ServerFacade.login(username, password);
+            if (loginResult instanceof LoginResponse loginResponse) {
+                String authToken = loginResponse.authToken();
+                PostloginUI postloginUI = new PostloginUI("http://localhost:8080", authToken);
+                OUT.println("Logging you in...");
+                postloginUI.run();
+            } else {
+                MessageResponse messageResponse = (MessageResponse) loginResult;
+                OUT.println(messageResponse.message());
+                OUT.println();
+                OUT.println(help());
+                OUT.println();
+                OUT.println("Enter command below.");
+            }
+        } catch (IOException e) {
+            OUT.println(e.getMessage());
+        }
     }
 
     public void quit() {
-
+        OUT.println("Thank you for playing...");
+        System.exit(0);
     }
 
 
