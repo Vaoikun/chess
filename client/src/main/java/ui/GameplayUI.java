2032283d3f532eadde5c4eb8e636cd1d3534200b
Vaoikun <furious_sesame_0o@icloud.com>
2025-08-09 03:57:57 -0600

    gameplay methods

--- client/src/main/java/ui/GameplayUI.java
+++ client/src/main/java/ui/GameplayUI.java
@@ -1,11 +1,12 @@
 package ui;
 
-import chess.ChessBoard;
-import chess.ChessGame;
+import chess.*;
 import com.google.gson.Gson;
 
 import java.io.PrintStream;
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Objects;
 import java.util.Scanner;
 
@@ -53,11 +54,101 @@ public class GameplayUI {
     }
 
     public void makeMove() {
+        try {
+            OUT.println("Select a piece. (Enter coordinate.)");
+            String input = SCANNER.nextLine();
+            ArrayList<Integer> coordinate = coordinateConverter(input);
+            int col = coordinate.get(0);
+            int row = coordinate.get(1);
+            ChessPosition chessPosition = new ChessPosition(row, col);
+            OUT.println("Select an end position. (Enter coordinate.)");
+            String endInput = SCANNER.nextLine();
+            ArrayList<Integer> endCoordinate = coordinateConverter(endInput);
+            int endCol = endCoordinate.get(0);
+            int endRow = endCoordinate.get(1);
+            ChessPosition endPosition = new ChessPosition(endRow, endCol);
+            ChessGame chessGame = webSocketFacade.chessGame;
+            ChessBoard chessBoard = chessGame.getBoard();
+            ChessPiece movingPiece = chessBoard.getPiece(chessPosition);
+            ChessPiece targetPiece = chessBoard.getPiece(endPosition);
+            ChessPiece.PieceType promotionType = null;
+            try {
+                if ((chessBoard.getPiece(chessPosition)).getPieceType() == ChessPiece.PieceType.PAWN && endRow == 8) {
+                    OUT.println("Enter a promotion type below. (e.g. QUEEN)");
+                    String promotionInput = SCANNER.nextLine();
+                    promotionType = promotion(promotionInput);
+                }
+            } catch (Exception e) {
+                System.out.println("Error: Invalid piece type.");
+            }
+            ChessMove chessMove = new ChessMove(chessPosition, endPosition, promotionType);
+            chessGame.makeMove(chessMove);
+            webSocketFacade.makeMove(authToken, gameID, chessMove);
+            reload();
+        } catch (Exception e) {
+            System.out.println("Error: Invalid piece position.");
+        }
+    }
 
+    public ChessPiece.PieceType promotion (String promotionInput) {
+        promotionInput = promotionInput.toUpperCase();
+        ChessPiece.PieceType promotionType = null;
+        switch (promotionInput) {
+            case "QUEEN" -> promotionType = ChessPiece.PieceType.QUEEN;
+            case "BISHOP" -> promotionType = ChessPiece.PieceType.BISHOP;
+            case "ROOK" -> promotionType = ChessPiece.PieceType.ROOK;
+            case "KNIGHT" -> promotionType = ChessPiece.PieceType.KNIGHT;
+            default -> promotionType = ChessPiece.PieceType.PAWN;
+        }
+        return promotionType;
     }
 
     public void highlight () {
+        OUT.println("Select a piece. (Enter coordinate.)");
+        String piecePosition = SCANNER.nextLine();
+        ArrayList<Integer> coordinate = coordinateConverter(piecePosition);
+        int col = coordinate.get(0);
+        int row = coordinate.get(1);
+        ChessPosition chessPosition = new ChessPosition(row, col);
+        ChessGame chessGame = webSocketFacade.chessGame;
+        ChessBoard chessBoard = chessGame.getBoard();
+        ChessPiece chessPiece = chessBoard.getPiece(chessPosition);
+        Collection<ChessMove> legalMoves = chessGame.validMoves(chessPosition);
+        if (teamColor == ChessGame.TeamColor.BLACK) {
+            BoardUI.callBlackTiles(OUT, chessBoard, legalMoves);
+        } else {
+            BoardUI.callWhiteTiles(OUT, chessBoard, legalMoves);
+        }
+    }
 
+    public static ArrayList<Integer> coordinateConverter (String input) {
+        Integer col = null;
+        Integer row = null;
+        ArrayList<Integer> coordinate = new ArrayList<>();
+        try {
+            row = Integer.parseInt(String.valueOf(input.charAt(1)));
+        } catch (NumberFormatException e) {
+            System.out.println("Error: Invalid row number.");
+        }
+        try {
+            String colInput = String.valueOf(input.charAt(2));
+            switch (colInput) {
+                case "a" -> col = 1;
+                case "b" -> col = 2;
+                case "c" -> col = 3;
+                case "d" -> col = 4;
+                case "e" -> col = 5;
+                case "f" -> col = 6;
+                case "g" -> col = 7;
+                case "h" -> col = 8;
+                default -> col = null;
+            }
+        } catch (Exception e) {
+            System.out.println("Error: Invalid column letter.");
+        }
+        coordinate.add(col);
+        coordinate.add(row);
+        return coordinate;
     }
 
     public void leave () {
